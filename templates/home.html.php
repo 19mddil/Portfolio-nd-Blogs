9ac5e7270127a1118528acac18b294c1b468ef4c
19mddil <mddilshadul98@gmail.com>
2020-09-21 02:38:09 +0600

    added some tabs and edits

--- templates/home.html.php
+++ templates/home.html.php
@@ -1,22 +1,34 @@
 
 <section style="position:sticky;position: -webkit-sticky; top: 0;" id="top">
+
 		<p style="background-color: rgba(0, 0, 0, 0.4);padding:4px;box-shadow: 2px 2px 7px black;border-radius: 4px 4px 4px 4px">
 		<!-- Want it on the right side -->
-		<i class="far fa-compass"></i>
-		<a href ="#preface" style="color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;"><strong>Preface</strong></a>&nbsp;
-		<a href ="#aboutme" style="color: white; text-decoration: underline;font-size: 14px;text-shadow: 1px 1px 2px #111111;"><strong>AboutMe</strong></a>&nbsp;
-		<a href ="#certificate" style="color: white; text-decoration: underline;font-size: 14px;text-shadow: 1px 1px 2px #111111;"><strong>My-Certificates</strong></a>
+			<i class="far fa-compass"></i>
+			<a href ="#preface" style="color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;">
+				<strong>Preface</strong>
+			</a>&nbsp;
+			<a href ="#aboutme" style="color: white; text-decoration: underline;font-size: 14px;text-shadow: 1px 1px 2px #111111;">
+				<strong>AboutMe</strong></a>&nbsp;
+			<a href ="#certificate" style="color: white; text-decoration: underline;font-size: 14px;text-shadow: 1px 1px 2px #111111;">
+				<strong>My-Certificates</strong>
+			</a>
 		</p>
+
 </section>
 <br>
 <article id="preface">
+
 	<h2>Preface</h2>
-	<p lang="bn">Hi,<strong style="color: green">Welcome to my Webpage.</strong>প্রোগ্রামিং এর জগৎে শেখার শেষ নাই। তাই এই শেখার রাস্তায় পুরানো টপিক ভুলারও শেষ নাই। তাই খুব ভালো হয় যা এক সময় খুব গুছিয়ে শিখেছি সেটা ওয়েব পেইজে লিখে রাখলে (৯০ এর HTML1 তৈরি করাই হয়েছিল সুন্দর সাইনটিফিক ডকুমেন্টেশন তৈরির নিয়তে, তাই ফায়দা কেনো লুটব না? )।
+	<p lang="bn">
+		Hi,<strong style="color: green">Welcome to my Webpage.</strong>প্রোগ্রামিং এর জগৎে শেখার শেষ নাই। তাই এই শেখার রাস্তায় পুরানো টপিক ভুলারও শেষ নাই। তাই খুব ভালো হয় যা এক সময় খুব গুছিয়ে শিখেছি সেটা ওয়েব পেইজে লিখে রাখলে (৯০ এর HTML1 তৈরি করাই হয়েছিল সুন্দর সাইনটিফিক ডকুমেন্টেশন তৈরির নিয়তে, তাই ফায়দা কেনো লুটব না? )।
 	</p>
+
 </article>
 <article id = "aboutme">
+
 	<h2>A brief about me</h2>
-	<p>Hi,I am software developer currently unemployed or without business:(.The deeper I dive into programming the more I discover new worlds which inspires me     to learn more and entreprenuerial notion guides my heart. My specialities are -
+	<p>
+		Hi,I am software developer currently unemployed or without business:(.The deeper I dive into programming the more I discover new worlds which inspires me     to learn more and entreprenuerial notion guides my heart. My specialities are -
 		    <ul>
 		      	<li>Writing scripts for client or server using python.(socket connection in different ports mostly 80)</li>
 		      	<li>Read json or xml data from REST API's(GeoCodeing API,Twitter API,Ouath maintain) and storing them in database.</li>
@@ -38,11 +50,14 @@
 			</ul>
 		    Also I have some fun with Java creating a beatbox music software and some multitheading MVC console applications,in addition at a time I was working with algorithms(Graph,Greedy,Sorting,String etc),progrming paradigms(complete search,dynamic programming,devide and conqure etc),data structes(stack,queue,map,binary search tree,Disjoint Sets etc) for becoming a better programmer using better data structure to solve different problems with C/C++ and developped some desktop application with database management system for swing(java) and .NET(c#) applications. Currently I am learning about agile development &amp; lean startup methodologies and applied data science. I am also woking on two personal projects one for my portfolio&amp;blogs and another for my ecommerce website on my shared hosting server which will be completed in two months schedule from which ecommerce website will be available in github pages.My Goal is learn software product management &amp; agile development so that I do not write code that nobody wants.
 	</p>
+
 </article>
 <article id="certificate" >
+
 	<h2>Certificates</h2>
 	<hr style="border:2px solid green">
 	<section style="border: 1px solid grey; padding: 2%;margin-bottom: 1%;">
+
 		<h3>Python</h3>
 		<p>
 			<a href="https://coursera.org/share/bf3d61e8ef284eb4e0590bfcfa309709">
@@ -66,8 +81,10 @@
 			</a>
 			<hr style="border:1px solid green">
 		</p>
+
 	</section>
 	<section style="border: 1px solid grey; padding: 2%;margin-bottom: 1%;">
+
 		<h3>Git &amp; Github</h3>
 		<p>
 			<hr style="border:1px solid green">
@@ -76,8 +93,10 @@
 			</a>
 			<hr style="border:1px solid green">
 		</p>
+
 	</section>
 	<section style="border: 1px solid grey; padding: 2%;margin-bottom: 1%;">
+
 		<h3>Responsive Web Desing</h3>
 		<p>
 			<hr style="border:1px solid green">
@@ -86,8 +105,10 @@
 			</a>
 			<hr style="border:1px solid green">
 		</p>
+
 	</section>
 	<section style="border: 1px solid grey; padding: 2%;margin-bottom: 1%;">
+
 		<h3>Django Framework</h3>
 		<p>
 		<hr style="border:1px solid green">
@@ -99,8 +120,8 @@
 		</a>
 		<hr style="border:1px solid green">
 		</p>
+		
 	</section>
-
 
 </article>
 
--- templates/layout.html.php
+++ templates/layout.html.php
@@ -1,10 +1,15 @@
 <!doctype html>
 <html>
 	<head>
+
 		<meta name="author" content="Md.Dilshadul Islam(19mddil/19mddilshad)">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="keywords" content="HTML, CSS, JavaScript,Responsive Web Desing,Django,Python,Applied Python Data Science,Version Control,Git,Github,Projects,19mddil,19mddilshad,Md.Dilshadul Islam,Certificates">
-		<meta name="description" content="This is my personal webpage where I save my projects advertisement for clients and my aquired djano &amp; php web framework,responsive web design,version control,devops,algorithm,agile_development,software poduct management knowledge and experience blogs.">
+		<meta 
+			name="keywords" 
+			content="HTML, CSS, JavaScript,Responsive Web Desing,Django,Python,Applied Python Data Science,Version Control,Git,Github,Projects,19mddil,19mddilshad,Md.Dilshadul Islam,Certificates">
+		<meta 
+			name="description" 
+			content="This is my personal webpage where I save my projects advertisement for clients and my aquired djano &amp; php web framework,responsive web design,version control,devops,algorithm,agile_development,software poduct management knowledge and experience blogs.">
 		<title> <?=$title?> </title>
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
@@ -13,12 +18,16 @@
 		<link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css2?family=Caveat&family=Sacramento&display=swap" rel="stylesheet">
 		<script src="https://kit.fontawesome.com/2c012d6c99.js" crossorigin="anonymous"></script>
+
 	</head>
 	<body style="font-family: monospace;background-color: #f5f5f5;color:black;font-size: 1.2em;border-radius: 4px 4px 4px 4px;">
-		
+
 		<main class="container">
+
 			<div class = "row">
+
 				<aside class = "col-md-2 one" style="padding: 0%;">
+
 					<script type="text/javascript">
 						(function() {
 				  			window.onresize = displayWindowSize;
@@ -40,70 +49,157 @@
 						})();
 					</script>
 					<details style="box-shadow: 2px 2px 7px black;background-color: black;" >
-					<summary style="">
-					<h1 style="font-weight:bold;font-family: 'Caveat', cursive; font-family: 'Sacramento', cursive;color: #c21e56;text-align: left; padding-left: 10px;"><i class="fas fa-bars"></i></h1>
-					</summary>
-					<!-- Bug -->
-					<nav style="font-weight: bold; text-align: left;padding-left: 5px;margin-right:3px;padding-right:3px;box-shadow: 2px 2px 7px #98fb98;border-radius: 4px 4px 4px 4px;" >
-						<hr>
-						<a href="index.php" style=" color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;"><i class="far fa-compass"></i>Home</a><br><hr>
-						<a href="project.php" style="color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;"><i class="far fa-compass"></i>My Projects</a><hr>
-						<a href="django.php"style="color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;"><i class="far fa-compass"></i>Django</a><hr>
-						<a href="version_control.php" style="color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;"><i class="far fa-compass"></i>Version Control</a><hr>
-						<a href="agile_development.php" style="color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;"><i class="far fa-compass"></i>Agile Development</a><hr>
-						<a href="software_product_management.php" style="color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;"><i class="far fa-compass"></i></i>Software Product Management</a><hr>
-						<a href="applied_data_science.php" style="color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;"><i class="far fa-compass"></i></i>Applied Data Science</a><hr>
-						<a href="installation_and_configuration.php" style="color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;" ><i class="far fa-compass"></i>Installation and Configuration</a><hr>
-						<a href="responsive_web_design.php" style="color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;"><i class="far fa-compass"></i>Resposive Web Design</a><br><br>
-					</nav>
+
+						<summary>
+
+							<h1 style="font-weight:bold;font-family: 'Caveat', cursive; 
+								font-family: 'Sacramento', cursive;
+								color: #c21e56;
+								text-align: left; 
+								padding-left: 10px;">
+								<i class="fas fa-bars"></i>
+							</h1>
+
+						</summary>
+						<!-- Bug -->
+						<nav style="font-weight: bold;
+							text-align: left;
+							padding-left: 5px;
+							margin-right:3px;
+							padding-right:3px;
+							box-shadow: 2px 2px 7px #98fb98;
+							border-radius: 4px 4px 4px 4px;" >
+
+							<hr>
+							<a href="index.php" style=" color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;">
+								<i class="far fa-compass"></i>Home
+							</a>
+							<hr>
+							<a href="project.php" style="color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;">
+								<i class="far fa-compass"></i>My Projects
+							</a>
+							<hr>
+							<a href="django.php" style="color: white; text-decoration: underline; font-size: 14px;text-shadow: 1px 1px 2px #111111;">
+								<i class="far fa-compass"></i>Django
+							</a>
+							<hr>
+							<a href="version_control.php" 
+								style="color: white;
+								text-decoration: underline;
+								font-size: 14px;
+								text-shadow: 1px 1px 2px #111111;">
+								<i class="far fa-compass"></i>Version Control
+							</a>
+							<hr>
+							<a href="agile_development.php" 
+								style="color: white;
+								text-decoration: underline;
+								font-size: 14px;
+								text-shadow: 1px 1px 2px #111111;">
+								<i class="far fa-compass"></i>Agile Development
+							</a>
+							<hr>
+							<a href="software_product_management.php" 
+								style="color: white; 
+								text-decoration: underline;
+								font-size: 14px;
+								text-shadow: 1px 1px 2px #111111;">
+								<i class="far fa-compass"></i>Software Product Management
+							</a>
+							<hr>
+							<a href="applied_data_science.php" 
+								style="color: white;
+								text-decoration: underline;
+								font-size: 14px;
+								text-shadow: 1px 1px 2px #111111;"
+								<i class="far fa-compass"></i>Applied Data Science
+							</a>
+							<hr>
+							<a href="installation_and_configuration.php" 
+								style="color: white;
+								text-decoration: underline;
+								font-size: 14px;
+								text-shadow: 1px 1px 2px #111111;" >
+								<i class="far fa-compass"></i>Installation and Configuration</a><hr>
+							<a href="responsive_web_design.php" 
+								style="color: white; text-decoration: underline;
+									font-size: 14px;
+									text-shadow: 1px 1px 2px #111111;">
+									<i class="far fa-compass"></i>Resposive Web Design
+							</a>
+							<br>
+							<br>
+
+						</nav>
+
 					</details>
+
 				</aside>
 
 				<section class = "col-md-10 two" style="background-color: white;color:black;padding: 0%">
-					<div id ="head" style="padding: 0%">
-						<h1 style="text-align:center;background-image: url('images/background.png');color:black;color: #c21e56;font-family: 'Montserrat', sans-serif;padding:2%;">MD's Programmed World<i class="fas fa-globe-americas" style="color: green"></i></h1>
-						<p style="text-align:center;font-family:monospace;font-size:.4em;color:red">(Beta version;final realease dued to october 5th)</p>
-					</div>
-					<h3 style="text-align: center;margin-bottom: 2%;"><q style="font-family: 'Caveat', cursive;
-					font-family: 'Sacramento', cursive;  color: #c21e56;text-align: center;"><q>The man with higher purpose,hard to stray from path</q></h3>
+
+					<header id ="head" style="padding: 0%">
+
+						<h1 
+							style="text-align:center;
+							background-image: url('images/background.png');
+							color: #c21e56;
+							font-family: 'Montserrat', sans-serif;padding:2%;">
+							MD's Programmed World<i class="fas fa-globe-americas" style="color: green"></i>
+						</h1>
+						<p style="text-align:center;font-family:monospace;font-size:.4em;color:red">
+							(Beta version;final realease dued to october 5th)
+						</p>
+
+					</header>
+					<h3 style="text-align: center;margin-bottom: 2%;">
+						<q style="font-family: 'Caveat', cursive; font-family: 'Sacramento', cursive;  color: #c21e56;text-align: center;">
+							The man with higher purpose,hard to stray from path
+						</q>
+					</h3>
 					<div style="margin-left: 10px;margin-right: 5px;">
-					<?= $output ?>
+						<?= $output ?>
 					</div>
-					<hr style="color:red;"><hr style="color:red;border: 2px solid green">
-					<p style="text-align: center;"><a href="#head"><i class="fas fa-chevron-up fa-5x"></i></a></p>
+					<hr style="color:red;">
+					<hr style="color:red;border: 2px solid green">
+					<p style="text-align: center;"> <a href="#head"> <i class="fas fa-chevron-up fa-5x"></i> </a> </p>
+
 				</section>
 				
 			</div>
+
 		</main>
 		<footer style=" background-image: url('images/background.png');color:black;padding: 3%; ">
+
 			<section >
-			<h3>Contact Me:</h3>
-				<a href="https://github.com/19mddil">
-					<img src="https://github.githubassets.com/favicons/favicon.png" width="25px" height="25px" 
-					title="Github"/>
-				</a>&nbsp;&nbsp;
-				<a href="https://stackoverflow.com/users/10853066/mohammed-dilshad">
-					<img src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196" width="25px" height="25px" title="Stackoverflow"/>
-				</a>&nbsp;&nbsp;
-				<a href="https://www.facebook.com/19womanizer">
-					<img src="https://static.xx.fbcdn.net/rsrc.php/yo/r/iRmz9lCMBD2.ico" width="25px" height="25px" title="Facebook"/></a>&nbsp;&nbsp;
-				<a href="https://www.linkedin.com/in/md-dilshadul-01b721199/">
-					<img src="https://static-exp1.licdn.com/sc/h/1bt1uwq5akv756knzdj4l6cdc" width="25px" height="25px" title="LinkedIn"/>
-				</a>&nbsp;&nbsp;
-				<a href="https://www.stopstalk.com/user/profile/19mddilshad">
-					<img src="https://www.stopstalk.com/stopstalk/static/images/fav_assets/favicon-32x32.png?_rev=20200425190226" width="25px" height="25px" title="Stopstalk"/>
-				</a>&nbsp;&nbsp;
-				<a href="https://www.hackerrank.com/mddilshadul98">
-					<img src="https://hrcdn.net/community-frontend/assets/favicon-ddc852f75a.png" width="25px" height="25px" title="Hackerrank"/>
-				</a>
+
+				<h3>Contact Me:</h3>
+					<a href="https://github.com/19mddil">
+						<img src="https://github.githubassets.com/favicons/favicon.png" width="25px" height="25px" title="Github"/>
+					</a>&nbsp;&nbsp;
+					<a href="https://stackoverflow.com/users/10853066/mohammed-dilshad">
+						<img src="https://cdn.sstatic.net/Sites/stackoverflow/Img/favicon.ico?v=ec617d715196" width="25px" height="25px" title="Stackoverflow"/>
+					</a>&nbsp;&nbsp;
+					<a href="https://www.facebook.com/19womanizer">
+						<img src="https://static.xx.fbcdn.net/rsrc.php/yo/r/iRmz9lCMBD2.ico" width="25px" height="25px" title="Facebook"/>
+					</a>&nbsp;&nbsp;
+					<a href="https://www.linkedin.com/in/md-dilshadul-01b721199/">
+						<img src="https://static-exp1.licdn.com/sc/h/1bt1uwq5akv756knzdj4l6cdc" width="25px" height="25px" title="LinkedIn"/>
+					</a>&nbsp;&nbsp;
+					<a href="https://www.stopstalk.com/user/profile/19mddilshad">
+						<img src="https://www.stopstalk.com/stopstalk/static/images/fav_assets/favicon-32x32.png?_rev=20200425190226" width="25px" height="25px" title="Stopstalk"/>
+					</a>&nbsp;&nbsp;
+					<a href="https://www.hackerrank.com/mddilshadul98">
+						<img src="https://hrcdn.net/community-frontend/assets/favicon-ddc852f75a.png" width="25px" height="25px" title="Hackerrank"/>
+					</a>
 			
 			</section>
 			<hr>
 			<p>Also You can mail me in my personal email account: <a href="mailto:mddilshadul98@19mddilshad.xyz">mddilshadul98@19mddilshad.xyz</a> </p>
 			<p>Or If you prefer Gmail: <a href="mailto:mddilshadul98@gmail.com">mddilshadul98@gmail.com</a></p>
-			<h2 style="text-align: center;">এমডির প্রোগ্রামিং জগৎ</h2>
-			<h2 style="text-align: center;font-family: 'Caveat', cursive;">Hope and work makes a man never look down on thee inside out.</h2>
+			<h2 style="text-align: center;font-family: 'Caveat', cursive;"><q>Hope and work makes a man never look down on thee inside out.</q></h2>
 			<h4 style="text-align: center;"><?php echo date('d-m-y') ?>(UTC)</h4>
+
 		</footer>
 	</body>
 </html>
